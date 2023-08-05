f2be50b7d7ec3b8c723551467d49555e2057fa0d
Rajnish <srajnish235.rs@gmail.com>
2023-08-05 12:05:59 +0530

    Adjusted Components
    Adjusted component to create a layout. Created And Applied CSS to display component.

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,17 +4,26 @@ import Header from "./Header";
 import General_Info from "./General_Info";
 import Education from "./Education";
 import Work from "./Work";
+import Display from "./Display";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const App = () => {
   return (
-    <div className="main_container">
-      <Header />
-      <General_Info />
-      <Education/>
-      <Work/>
+    <>
+    <Header/>
+    <div className="parent">
+      <div className="main_container">
+        <General_Info />
+        <Education />
+        <Work />
+      </div>
+
+      <div className="display-container">
+        <Display />
+      </div>
     </div>
+    </>
   );
 };
 
--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Display =()=>{
-    return(
-        <div>
-            
-        </div>
-    )
-}
+const Display = () => {
+  return(
+    <div className="display_MainContainer">
+        <h1>Hello</h1>
+    
+    </div>
+  )
+};
+
+export default Display;
